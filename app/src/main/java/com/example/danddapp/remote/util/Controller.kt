75033c63f0fd37f530aeee331f21e07512bab3ec
Jhonata Avila <joavilati@gmail.com.br>
2020-09-25 22:44:57 -0300

    feat(Controller): create controller, okhttp and retrofit  dependency.

--- app/src/main/java/com/example/danddapp/remote/util/Controller.kt
+++ app/src/main/java/com/example/danddapp/remote/util/Controller.kt
@@ -1,5 +1,41 @@
 package com.example.danddapp.remote.util
 
-class Controller: Interceptor {
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Response
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+private const val baseUrl = "https://www.dnd5eapi.co/api/"
+
+class Controller : Interceptor {
+
+    private val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor(getLoggingInterceptor())
+        .addNetworkInterceptor(this)
+        .connectTimeout(15, TimeUnit.SECONDS)
+        .writeTimeout(30, TimeUnit.SECONDS)
+        .readTimeout(30, TimeUnit.SECONDS)
+        .build()
+
+    val retrofit = Retrofit.Builder()
+        .client(okHttpClient)
+        .baseUrl(baseUrl)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    private fun getLoggingInterceptor(): HttpLoggingInterceptor {
+        val logging = HttpLoggingInterceptor()
+        logging.level = HttpLoggingInterceptor.Level.BODY
+        return logging
+    }
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val originalRequest = chain.request()
+        val newRequest = originalRequest.newBuilder().build()
+        return chain.proceed(newRequest)
+    }
 
 }
